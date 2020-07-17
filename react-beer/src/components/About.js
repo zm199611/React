da0b37d853ab4f260986d4442892586d1cbcfeea
zhuming <1109282075@qq.com>
2020-07-17 15:22:43 +0800

    啤酒售卖网页完善

---
+++ react-beer/src/components/About.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Header from './Header';
+class About extends React.Component {
+  constructor() {
+    super();
+  }
+  render() {
+    return (
+      <div >
+        <Header siteName="Beer me" />
+        关于我们
+      </div>
+    )
+  } 
+}
+
+export default About;
