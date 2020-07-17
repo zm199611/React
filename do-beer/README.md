f26b3d5de64ce1398127fc79b630ab9abcb8d109
zhuming <1109282075@qq.com>
2020-07-17 15:14:35 +0800

    售卖啤酒网页

---
+++ do-beer/README.md
@@ -0,0 +1,14 @@
+主题：  react beer 应用开发思路整理
+beer  搜索，  -》 列表  -》 点进去  创建路由
+
+3 个路由设计   beer 这三个路由
+/ RESTFUL 
+搜索根列表显示和二为一？ http://www.cesheng.com/search/:searchTerm?a=q&b=2&=c=3   /search/all  /search/8    params 
+详情页  /beer/:beerId     detail       details/item?id=
+语义化设计URL    /post/:postId
+
+1. 把应用架构搭好， 路由 
+2. 组件化思维
+3. list列表的编写流程
+4. router  this.props.match.params
+5. 首页 / -> Main.js ->loadBeer ->loading -> list
---
+++ do-beer/src/components/Main.js
@@ -0,0 +1,83 @@
+import React from "react";
+// StatefullComponent  StatelessComponent
+import Search from "./Search";
+import Results from "./Results";
+import Header from "./Header";
+
+class Main extends React.Component {
+  // 集列表， 搜索于一体 怎么做？ 
+  constructor() {
+    super();
+    this.state = {
+      beers: [],
+      loading: true // 正在加载中  体验
+    }
+  }
+  
+  // URL不一样？ 分析URL不一样
+  componentDidMount() {
+    // /  把所有啤酒都查出来， 
+    // /search/:searchTerm   就查searchTerm
+    // const \
+    console.log(this.props.match.params);
+    const params = this.props.match.params || {} // 
+    const searchTerm = params.searchTerm || undefined;
+    this.loadBeers(searchTerm); // 
+  }
+
+  componentDidUpdate(prevProps) {
+    // console.log('did update')
+    // console.log(prevProps);
+    const currentSearchTerm = this.props.match.params.searchTerm; // 新的参数
+    const oldSearchTerm = prevProps.match.params.searchTerm;
+    // console.log(oldSearchTerm, currentSearchTerm);
+    if (currentSearchTerm !== oldSearchTerm) {
+      this.loadBeers(currentSearchTerm)
+    }
+  }
+  // = hops 在业务上有什么用？
+  // es6 概念 默认赋值
+  loadBeers(searchTerm = "hops") {
+    const localStorageBeers = localStorage.getItem(`search-${searchTerm}`) 
+    if (localStorageBeers) {
+      const localBeers = JSON.parse(localStorageBeers);
+      this.setState({
+        beers: localBeers,
+        loading: false
+      })
+      return ;
+    }
+    fetch(`http://api.react.beer/v2/search?q=${searchTerm}&type=beer`) // api 地址
+      .then(data => data.json())
+      .then(data => {
+        
+        const beers = data.data || [];
+        
+        this.setState({
+          loading: false,
+          beers
+        });
+        // 业务
+        // 列表记录相关 searchTerm 变化
+        localStorage.setItem(
+          `search-${searchTerm}`,
+          JSON.stringify(this.state.beers)
+        )
+        console.log(data)
+      })
+  }
+
+  render() {
+    return (
+      <div>
+        <Header siteName="Beer me!" />
+        <Search /> 
+        {/* 搜索组件 */}
+        <Results beers={this.state.beers}
+         loading={this.state.loading} />
+      </div>
+    )
+  }
+}
+
+export default Main
