d3477a51a5136c45179bdd11fb14e364609cacca
zhuming <1109282075@qq.com>
2020-07-22 11:40:38 +0800

    style组件化思想

--- sanyuan-music/src/index.js
+++ sanyuan-music/src/index.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { HashRouter } from 'react-router-dom'
-import { renderRoutes } from 'react-router-config'
-import routes from './routes/index'
+import { HashRouter } from "react-router-dom";
+import { renderRoutes } from "react-router-config";
+import  routes from "./routes/index.js";
+import { Provider } from 'react-redux';
+import store from './store';
 
 ReactDOM.render(
   <React.StrictMode>
-    <HashRouter>
-      {renderRoutes(routes)}
-    </HashRouter>
+    <Provider store={store}>
+      <HashRouter>
+        { renderRoutes(routes) }
+      </HashRouter> 
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
